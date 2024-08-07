0b24b91d20aca7cb2e82772d7994d8f28e9d4fd9
hamzaejaz787 <hejaz389@gmail.com>
2024-08-07 15:29:28 +0500

    updated gallery and business unit content types

--- src/api/gallery/controllers/gallery.js
+++ src/api/gallery/controllers/gallery.js
@@ -11,6 +11,13 @@ module.exports = createCoreController("api::gallery.gallery", ({ strapi }) => ({
     const { id } = ctx.params;
     const entity = await strapi.db.query("api::gallery.gallery").findOne({
       where: { slug: id },
+      populate: {
+        cover: { fields: ["name", "url", "alternativeText"] },
+        GalleryItems: { fields: ["name", "url", "alternativeText"] },
+      },
     });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
   },
 }));
